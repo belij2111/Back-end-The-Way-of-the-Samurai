1231ece8f0e92a5bbeda1d7bb9501ffc35821ae8
belij2111 <belijvalerij2111@gmail.com>
2024-03-16 17:58:19 +0300

    feat(server-06.js): Apply asynchronous flow

--- Lesson-06/server-06.js
+++ Lesson-06/server-06.js
@@ -3,19 +3,17 @@ const http = require('http')
 const server = http.createServer((request, response) => {
     switch (request.url) {
         case '/home': {
-            const start = new Date()
-            while (new Date() - start < 5000) {
-                console.log(new Date() - start)
-            }
-            const data = 'best free online course'
-            response.write(data)
-            response.end()
+            setTimeout(() => {
+                const data = 'best free online course'
+                response.write(data)
+                response.end()
+            }, 10000)
             break
         }
-        default:
+        default: {
             response.write('404 not found')
             response.end()
-
+        }
     }
 })
 server.listen(3003)
